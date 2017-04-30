b09803260fafcee5411d512caffa6d8f2744a63c
Joel Herb <he_qiao_2010@yeah.net>
2017-04-30 13:42:42 +0800

    update digest demo.
    Change-Id: Iabe5900f6c1db728d227ea81ebb7cad44d31557f
    Signed-off-by: Joel Herb <he_qiao_2010@yeah.net>

---
+++ DigestTest/src/com/github/heqiao2010/HTTPDigestClientDemo.java
@@ -0,0 +1,33 @@
+package com.github.heqiao2010;
+
+import java.io.IOException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.ParseException;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.util.EntityUtils;
+
+/**
+ * @author heqiao
+ * 
+ */
+public class HTTPDigestClientDemo {
+
+
+	public static void main(String args[]) throws ParseException, IOException {
+		String postData = "{\"nas_id\": 355957,\"user_mac\": "
+				+ "\"0C-D7-46-0D-98-E8\",\"session_timeout\": 123,"
+				+ "\"identifier\": \"123\",\"redirect_uri\": \"https://10.168.168.1/\","
+				+ "\"result_url\": \"https://www.baidu.com/\" "
+				+ ",\"ssid\":\"Chimelong\"}";
+		System.out.println("postData: " + postData);
+		HttpPost req = new HttpPost("http://oasisauth.h3c.com/portal/authMac.auth");
+		StringEntity se = new StringEntity(postData, "UTF-8");
+		req.setHeader("Content-Type", "application/json");
+		req.setHeader("Accept", "application/json");
+		req.setEntity(se);
+		HttpResponse rep = HttpDigestUtils.send("XXX", "XXX", req);
+		System.out.println("rep: " + EntityUtils.toString(rep.getEntity()));
+	}
+}
---
+++ DigestTest/src/com/github/heqiao2010/HttpDigestUtils.java
@@ -0,0 +1,46 @@
+package com.github.heqiao2010;
+
+import java.net.URI;
+import java.util.Collections;
+
+import org.apache.commons.httpclient.auth.AuthPolicy;
+import org.apache.http.HttpResponse;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.Credentials;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.auth.DigestSchemeFactory;
+import org.apache.http.impl.client.DefaultHttpClient;
+/**
+ * http digest工具类
+ * @author heqiao
+ *
+ */
+public class HttpDigestUtils {
+	/**
+	 * http client
+	 */
+	private static DefaultHttpClient httpClient = new DefaultHttpClient();
+	
+	/**
+	 * 发送摘要认证请求
+	 */
+	@SuppressWarnings("deprecation")
+	public static HttpResponse send(String username, String password, HttpUriRequest request){
+		HttpResponse response = null;
+		try {
+			URI serverURI = request.getURI();
+			Credentials creds = new UsernamePasswordCredentials(username, password);
+			httpClient.getCredentialsProvider().setCredentials(new AuthScope(serverURI.getHost(), serverURI.getPort()),
+					(Credentials) creds);
+			httpClient.getParams().setParameter(AuthPolicy.AUTH_SCHEME_PRIORITY,
+					Collections.singleton(AuthPolicy.DIGEST));
+			httpClient.getAuthSchemes().register(AuthPolicy.DIGEST, new DigestSchemeFactory());
+			response = httpClient.execute(request);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return response;
+	}
+	
+}
